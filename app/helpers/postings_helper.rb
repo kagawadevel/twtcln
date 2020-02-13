81974a5c1e6f343162890894f07b15136922879a
kakegawa <cmuwni@gmail.com>
2020-02-13 22:03:00 +0900

    新規投稿画面を作成

--- app/helpers/postings_helper.rb
+++ app/helpers/postings_helper.rb
@@ -1,2 +1,9 @@
 module PostingsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm'
+      confirm_postings_path
+    elsif action_name == 'edit'
+      posting_path
+    end
+  end
 end
